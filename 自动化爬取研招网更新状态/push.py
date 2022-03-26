0c6e68d90857a302a88db3713c6fe5eb9e0bb0b5
KID <80014289+Mr-KID-github@users.noreply.github.com>
2022-03-26 19:29:08 +0800

    Update push.py
    添加研招网未更新推送

--- 自动化爬取研招网更新状态/push.py
+++ 自动化爬取研招网更新状态/push.py
@@ -6,15 +6,25 @@
 import requests
 import json
 
-def push():
-    datas = json.dumps({
-                    "appToken":"AT_JoGFbfbWLR3D7n0HaWraUDd92lSIwxBy",
-                    "content":"研招网更新了！快去看看是不是你需要的内容！",
-                    "contentType": 1,
-                    "summary":"研招网更新了!",
-                    "uids":["UID_JFMEhBSDqKj7AjaILStgmrDi8kOb"],
-                    "url":"https://yz.chsi.com.cn/kyzx/kydt/?start=0"
-                })
+def push(status):
+    if status:
+        datas = json.dumps({
+                        "appToken":"AT_JoGFbfbWLR3D7n0HaWraUDd92lSIwxBy",
+                        "content":"研招网更新了！快去看看是不是你需要的内容！",
+                        "contentType": 1,
+                        "summary":"研招网更新了!",
+                        "uids":["UID_JFMEhBSDqKj7AjaILStgmrDi8kOb","UID_TIh9a2iXJi6vwDf8Nj4zq3MVySY3"],
+                        "url":"https://yz.chsi.com.cn/kyzx/kydt/?start=0"
+                    })
+    else:
+        datas = json.dumps({
+            "appToken": "AT_JoGFbfbWLR3D7n0HaWraUDd92lSIwxBy",
+            "content": "呜呜呜！研招网还没有更新，请耐心等待！！！",
+            "contentType": 1,
+            "summary": "研招网还没有更新",
+            "uids": ["UID_JFMEhBSDqKj7AjaILStgmrDi8kOb", "UID_TIh9a2iXJi6vwDf8Nj4zq3MVySY3"],
+            "url": "https://yz.chsi.com.cn/kyzx/kydt/?start=0"
+        })
     headers = {'Content-Type': 'application/json'}
     r = requests.post("http://wxpusher.zjiecode.com/api/send/message", data=datas,headers=headers)
     print(r.text)
